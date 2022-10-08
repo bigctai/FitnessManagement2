dacf486ab3ae2d95f62640f476475afe966d8ba7
Chris Tai <christai100154@gmail.com>
2022-10-08 02:08:20 -0400

    Updated constructors and toString methods

--- src/projecttwo/Premium.java
+++ src/projecttwo/Premium.java
@@ -3,11 +3,15 @@ package projecttwo;
 public class Premium extends Family{
 
     private static final double MEMBERSHIP_FEE = 0;
-    public Premium(String fname, String lname, Date dob, Date expire, Location location){
-        super(fname, lname, dob, expire, location);
+    public Premium(String fname, String lname, Date dob, Date expire, Location location, int guestPass){
+        super(fname, lname, dob, expire, location, guestPass);
     }
     @Override
     public double membershipFee(){
         return MEMBERSHIP_FEE;
+    }
+
+    public String toString(){
+        return super.toString();
     }
 }
