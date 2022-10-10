1161ed85c73558ad920d297ce6a10b7fffe7deca
Chris Tai <christai100154@gmail.com>
2022-10-10 03:39:19 -0400

    Updated ClassSchedule and fixed bugs

--- src/projecttwo/ClassSchedule.java
+++ src/projecttwo/ClassSchedule.java
@@ -18,6 +18,20 @@ public class ClassSchedule {
         return classes[indexOfClass];
     }
 
+    public int getNumOfClasses(){
+        return numOfClasses;
+    }
+
+    public FitnessClass find(String instructor, Location location, String className){
+        for(int i = 0; i<numOfClasses; i++){
+            if(classes[i].getInstructor().equals(instructor) && classes[i].getLocation().equals(location) &&
+                classes[i].getClass().equals(className)){
+                return classes[i];
+            }
+        }
+        return null;
+    }
+
     public void addClass(FitnessClass fitClass){
         classes[numOfClasses] = fitClass;
     }
